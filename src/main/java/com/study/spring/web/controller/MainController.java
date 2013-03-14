f8a3091af858967b8cf78a991eac5a560704296b
WuXiaozhe <justfordev.wu@gmail.com>
2013-03-14 21:47:21 +0800

    [xzwu] add spring mvc configures

--- src/main/java/com/study/spring/web/controller/MainController.java
+++ src/main/java/com/study/spring/web/controller/MainController.java
@@ -1,5 +1,20 @@
 package com.study.spring.web.controller;
 
-public class MainController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+public class MainController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView("welcome");
+		mv.addObject("name", "WangFan");
+		return mv;
+	}
+	 
 }
